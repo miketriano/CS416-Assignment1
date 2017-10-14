c2325393d17f5bf12a74ca5ed679412f1263deb9
miketriano <mtriano@outlook.com>
2017-10-14 18:43:09 -0400

    added mainContext and changed setcontext

--- my_pthread.c
+++ my_pthread.c
@@ -11,20 +11,31 @@
 #define MEM 64000
 #define THREAD_NUM 10
 
+ucontext_t * mainContext;
+int initialized = 0;
 int currentThread = 0;
 tcb threadBlocks[THREAD_NUM];
 
 /* create a new thread */
 int my_pthread_create(my_pthread_t * thread, pthread_attr_t * attr, void *(*function)(void*), void * arg) {
 	
+	if (!initialized) {
+		initialized = 1;
+		mainContext = malloc(sizeof(ucontext_t));
+		getcontext(mainContext);
+	}
+	
+	printf("Create thread %p\n", thread);
+	
 	tcb * threadBlock;
 	threadBlock = malloc(sizeof(tcb));
+	thread = malloc(sizeof(my_pthread_t));
 	threadBlock->pthread = thread;
 	
 	// create context for thread
 	threadBlock->context = malloc(sizeof(ucontext_t));
 	getcontext(threadBlock->context);
-	threadBlock->context->uc_link = 0;
+	threadBlock->context->uc_link = mainContext;
 	threadBlock->context->uc_stack.ss_sp = malloc(MEM);
 	threadBlock->context->uc_stack.ss_size = MEM;
 	threadBlock->context->uc_stack.ss_flags = 0;
@@ -51,9 +62,10 @@ void my_pthread_exit(void *value_ptr) {
 /* wait for thread termination */
 int my_pthread_join(my_pthread_t thread, void **value_ptr) {
 	
-	printf("Join called on thread %d\n", thread);
-	
-	setcontext(threadBlocks[thread].context);
+	printf("Join called on thread %p\n", &thread);
+		
+	setcontext(threadBlocks[currentThread].context);
+	currentThread++;
 	
 	return 0;
 };
